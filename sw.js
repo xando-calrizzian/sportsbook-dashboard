87163cef3713ebdb6b84a5bbd66d9cf2332d6307
Alexander Franshaw <alexander@telosiangrowth.com>
2026-06-23 18:50:33 -0500

    dashboard: 2026-06-23 23:50 UTC [private-placeholder]

--- sw.js
+++ sw.js
@@ -14,7 +14,7 @@
 // CACHE_VERSION is substituted by deploy_dashboard.py at push time so
 // browsers see this as a "new" SW worth installing.
 
-const CACHE_VERSION = "v1782232754";
+const CACHE_VERSION = "v1782258633";
 
 self.addEventListener("install", (event) => {
   event.waitUntil(
